c0760509acfd0f1d17c97fe2576a02c3f3d5d55a
Ibgi <yavrosman@gmail.com>
2025-02-28 13:07:30 +0200

    sd

--- src/App.vue
+++ src/App.vue
@@ -7,5 +7,26 @@ import GameCanvas from './components/GameCanvas.vue';
 </script>
 
 <style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 
+html,
+body {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+
+#app {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  overflow: auto;
+  background-color: #f9f9f9;
+  font-family: Arial, sans-serif;
+}
 </style>
