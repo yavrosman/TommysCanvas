d8176ab630c124346edab74593b2cc4e35a9908b
Ibgi <yavrosman@gmail.com>
2025-02-28 13:03:47 +0200

    Initial commit connecting local Vue project

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <GameCanvas />
+</template>
+
+<script setup>
+import GameCanvas from './components/GameCanvas.vue';
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/GameCanvas.vue
@@ -0,0 +1,28 @@
+<template>
+    <canvas ref="canvas"></canvas>
+</template>
+  
+<script setup>
+import { onMounted, ref } from 'vue';
+
+const canvas = ref(null);
+
+onMounted(() => {
+    const ctx = canvas.value.getContext('2d');
+    canvas.value.width = window.innerWidth;
+    canvas.value.height = window.innerHeight;
+
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, canvas.value.width, canvas.value.height);
+});
+
+</script>
+
+<style scoped>
+canvas {
+    display: block;
+    width: 100%;
+    height: 100%;
+}
+</style>
+  
